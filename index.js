850059f7f2414c0f7e8dd10187b83d6a37ab0811
Udit Kumar <surajgautam783792@gmail.com>
2024-02-15 22:29:07 +0530

    Adding Conditions And Statements

---
+++ index.js
@@ -0,0 +1,59 @@
+// What is Conditions
+// To Implement Some Condition in the code (In Simple Language Making Logics)
+
+// If Statements And Else-----------------------??????
+// let age = 15;
+// if (age > 25) {
+//   console.log("You Can Visit In This Website");
+// }
+// if (age < 25) {
+//   console.log("You Cannot Visit In This Website");
+// }
+
+// You can also use if condition multiple time When You Have Multiple Logics
+// OtherWise You Can Use Else Conditions Statements
+
+// // Odd Even Khel ------------------->>>
+// let num = 3;
+// if (num % 2 === 0) {
+//   console.log(num, "Even Numbers");
+// } else {
+//   console.log(num, "Odd Number");
+// }
+
+// Else-If Statement -----------------------??????No Need To Write If Statment Again And Again
+
+// let name = "Rohit";
+// if (name === "Jai") {
+//   console.log("Welcome Jai");
+// } else if (name === "Udit") {
+//   console.log("Welcome Udit");
+// } else if (name === "Rohit") {
+//   console.log("Welcome Rohit");
+// } else {
+//   console.log("You Are Unknown Person");
+// }
+
+// Ternary Operators---------------------?????????????????
+// Condition ? True Output :False Output
+// let age = 18;
+// age > 15 ? "You Can Drive" : "You Doesn't";
+
+// This Is Not Executed To Print
+
+// -------1st Method To Print Using Variables
+// let age = 18;
+// let result = age > 15 ? "You Can Drive" : "You Doesn't";
+// console.log(result);
+
+// -------2nd Method To Print
+// let age = 18;
+// age > 15 ? console.log("You Can Drive") : console.log("You Doesn't");
+
+// Task-
+// Write A Code Which Can Gives Grades to Students According To Their Scores
+// a-> 90-100--A
+// b-> 70-89--B
+// c-> 60-69--C
+// d-> 50-59--D
+// e-> 0-49--E
